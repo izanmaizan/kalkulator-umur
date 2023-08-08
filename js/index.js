ec18dc0563efdf1c5d86cbe89afac59da27eede1
izanmaizan <izanmaizan@gmail.com>
2023-08-08 20:34:13 +0700

    memperbaiki

--- js/index.js
+++ js/index.js
@@ -20,18 +20,18 @@ const validateInputValue = () => {
     errorSpan[0].textContent = errorInputDate["errorEmptyDate"]
     errorSpan[0].classList.add("active");
     calculatorInput[0].classList.add('input-error')
-    dateSpan[0].classList.add('calculator-input__errorDate')
+    dateSpan[0].classList.add('calcInput-errorDate')
     errorInputDay = true
   } else if (calculatorInput[0].value < 1 || calculatorInput[0].value > 31) {
     errorSpan[0].textContent = errorInputDate["errorInvalidDate"]
     errorSpan[0].classList.add("active");
     calculatorInput[0].classList.add('input-error')
-    dateSpan[0].classList.add('calculator-input__errorDate')
+    dateSpan[0].classList.add('calcInput-errorDate')
     errorInputDay = true
   } else {
     errorSpan[0].classList.remove("active");
     calculatorInput[0].classList.remove('input-error')
-    dateSpan[0].classList.remove('calculator-input__errorDate')
+    dateSpan[0].classList.remove('calcInput-errorDate')
     errorInputDay = false
   }
 
@@ -39,18 +39,18 @@ const validateInputValue = () => {
     errorSpan[1].textContent = errorInputDate["errorEmptyDate"]
     errorSpan[1].classList.add("active");
     calculatorInput[1].classList.add('input-error')
-    dateSpan[1].classList.add('calculator-input__errorDate')
+    dateSpan[1].classList.add('calcInput-errorDate')
     errorInputMonth = true
   } else if (calculatorInput[1].value < 1 || calculatorInput[1].value > 12) {
     errorSpan[1].textContent = errorInputDate["errorInvalidDate"]
     errorSpan[1].classList.add("active");
     calculatorInput[1].classList.add('input-error')
-    dateSpan[1].classList.add('calculator-input__errorDate')
+    dateSpan[1].classList.add('calcInput-errorDate')
     errorInputMonth = true
   } else {
     errorSpan[1].classList.remove("active");
     calculatorInput[1].classList.remove('input-error')
-    dateSpan[1].classList.remove('calculator-input__errorDate')
+    dateSpan[1].classList.remove('calcInput-errorDate')
     errorInputMonth = false
   }
 
@@ -58,18 +58,18 @@ const validateInputValue = () => {
     errorSpan[2].textContent = errorInputDate["errorEmptyDate"]
     errorSpan[2].classList.add("active");
     calculatorInput[2].classList.add('input-error')
-    dateSpan[2].classList.add('calculator-input__errorDate')
+    dateSpan[2].classList.add('calcInput-errorDate')
     errorInputYear = true
   } else if (calculatorInput[2].value < 1 || calculatorInput[2].value > date.getFullYear()) {
     errorSpan[2].textContent = errorInputDate["errorInvalidDate"]
     errorSpan[2].classList.add("active");
     calculatorInput[2].classList.add('input-error')
-    dateSpan[2].classList.add('calculator-input__errorDate')
+    dateSpan[2].classList.add('calcInput-errorDate')
     errorInputYear = true
   } else {
     errorSpan[2].classList.remove("active");
     calculatorInput[2].classList.remove('input-error')
-    dateSpan[2].classList.remove('calculator-input__errorDate')
+    dateSpan[2].classList.remove('calcInput-errorDate')
     errorInputYear = false
   }
 
--- js/nodes.js
+++ js/nodes.js
@@ -6,12 +6,12 @@ const $ = (element) => {
     return document.querySelectorAll(element);
   }
   
-  const calculatorForm = $('#calculator-form')
+  const calculatorForm = $('#calc-form')
   const dayInput = $('#day-input')
   const monthInput = $('#month-input')
   const yearInput = $('#year-input')
   const ageDaysSpan = $('#days-age')
   const ageMonthsSpan = $('#months-age')
   const ageYearsSpan = $('#years-age')
-  const errorSpan = $All('.calculator-input__error')
-  const dateSpan = $All('.calculator-input__date');
+  const errorSpan = $All('.calcInput-error')
+  const dateSpan = $All('.calcInput-date');
